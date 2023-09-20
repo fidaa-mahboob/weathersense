9b3f825210e6e10091715bcf426dd4ea0e2a0cdd
mohammed.mahboob1 <mohammed.mahboob1@hscic.gov.uk>
2023-09-20 12:12:48 +0100

    Added Open Weather API url fetch to acquire json data through either postcode or location name

--- src/App.js
+++ src/App.js
@@ -3,13 +3,33 @@ import './App.css';
 import LocationSearch from './pages/LocationSearch'
 import LocationWeather from './pages/LocationWeather'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import useLocationData from './hook/useLocationData';
+import { useEffect } from 'react';
 
 function App() {
+  const [locationName, coordinates, postCode, setCoordinates, setLocationName, setPostCode] = useLocationData(null)
+
+  useEffect(() => {
+    if(locationName !== ''){
+      fetch("http://api.openweathermap.org/geo/1.0/direct?q="+ {locationName} + "&limit=5&appid={API key}")
+      .then((data) => data.json)
+      .then((json) => setCoordinates([json.lat, json.lon]))
+      .catch((e) => console.log(e))
+      setLocationName('')
+    } else if (postCode !== ''){
+      fetch("http://api.openweathermap.org/geo/1.0/zip?zip=" + {postCode} + "&appid={API key}")
+      .then((data) => data.json)
+      .then((json) => setCoordinates([json.lat, json.lon]))
+      .catch((e) => console.log(e))
+      setPostCode('')
+    }
+    }, [locationName, postCode, setCoordinates, setLocationName, setPostCode])
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LocationSearch />} />
-        <Route path="/Location/Weather/Data" element={<LocationWeather/>}/>
+        <Route path="/" element={<LocationSearch setLocationName={setLocationName} setPostCode={setPostCode}/>} />
+        <Route path="/Location/Weather/Data" element={<LocationWeather coordinates={coordinates}/>}/>
       </Routes>
     </BrowserRouter>
   )
