e7a02b2f65fe762b3cc302ed5425e2c2acdf8955
mohammed.mahboob1 <mohammed.mahboob1@hscic.gov.uk>
2023-09-20 12:09:23 +0100

    Initialised react components for weather app

---
+++ src/components/DailyWeather.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DailyWeather = () => {
+  return (
+    <div>DailyWeather</div>
+  )
+}
+
+export default DailyWeather
---
+++ src/components/LocationWeatherSummary.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LocationWeatherSummary = () => {
+  return (
+    <div>LocationWeatherSummary</div>
+  )
+}
+
+export default LocationWeatherSummary
