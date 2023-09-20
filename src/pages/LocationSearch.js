fe7c943bf893e383a0141d12c622251b7be485da
mohammed.mahboob1 <mohammed.mahboob1@hscic.gov.uk>
2023-09-20 12:10:50 +0100

    Added react search component to location search page

--- src/pages/LocationSearch.js
+++ src/pages/LocationSearch.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import LocationWeatherSearch from '../components/LocationWeatherSearch'
 
-const LocationSearch = () => {
+const LocationSearch = ({}) => {
   return (
     <LocationWeatherSearch/>
   )
