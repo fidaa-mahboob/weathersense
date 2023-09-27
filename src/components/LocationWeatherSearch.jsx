fce15236a1fd8ff260a971f0465ae2490f7c8e3a
mohammed.mahboob1 <mohammed.mahboob1@hscic.gov.uk>
2023-09-27 12:05:39 +0100

    Removed commented code

--- src/components/LocationWeatherSearch.jsx
+++ src/components/LocationWeatherSearch.jsx
@@ -3,36 +3,7 @@ import { useNavigate } from 'react-router-dom'
 
 const LocationWeatherSearch = ({fetchCoordinates, setInput, input}) => {
   
-  
   let searchButton
-
-  // const validPostCodeFormatAndExists = (postCode) => {
-  //   if(/^[A-Za-z]{1,2}[0-9]{1,2}[A-Za-z]?[0-9][A-Za-z]{2}$/.test(postCode)){
-  //     fetch("https://api.postcodes.io/postcodes/" + postCode + "/validate")
-  //     .then(data => data.json())
-  //     .then(json => setPostCodeExists(json.result))
-  //     console.log("test1")
-  //     return postCodeExists
-  //   }else{
-  //     return false
-  //   }
-  // }
-
-  // const handleClick = () => {
-  //   if(validPostCodeFormatAndExists(input)){
-  //     setPostCode(input)
-  //     console.log("using postcode")    
-  //     } else {
-  //     setLocationName(input)
-  //     console.log("using location name")
-  //   }
-  // }
-
-  // const handleCurrentLocationButtonClick = () => {
-  //   console.log("current location button clicked")
-  //   console.log(validPostCodeFormatAndExists("wf133ag"))
-  // }
-
 
   if(input === ''){
     searchButton = <button type="submit" disabled>Search</button>
