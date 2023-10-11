801842616e3072f0b7abaf25be6dc36dff252ad0
mohammed.mahboob1 <mohammed.mahboob1@hscic.gov.uk>
2023-10-11 12:49:08 +0100

    Basic design for Search page has been completed

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,27 +1,36 @@
 import React from 'react'
 import Inputs from './Inputs'
-import UilReact from '@iconscout/react-unicons/icons/uil-react'
+import { UilSearch, UilLocationPinAlt  } from '@iconscout/react-unicons'
+import WeatherSenseLogo from '../assets/images/WeatherSense.png'
 
 const LocationWeatherSearch = ({ fetchCoordinates, setInput, input }) => {
 
   let searchButton
 
   if (input === '') {
-    searchButton = <button type="submit" disabled>Search</button>
+    searchButton = <button className="px-4 py-2 rounded bg-gray-300 transition-all duration-500 hover:shadow-md" type="submit" disabled>Search</button>
   } else {
-    searchButton = <button type="submit">Search</button>
+    searchButton = <button className="px-4 py-2 rounded bg-gray-300 transition-all duration-500 hover:shadow-md"  type="submit">Search</button>
   }
 
   return (
-    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
-      <Inputs />
-      <form onSubmit={fetchCoordinates}>
-        <div>
-          <input placeholder="Enter location or postcode" type="text" onChange={(e) => setInput(e.target.value)} />
+    <div className='container px-4 mx-auto pt-24 mt-8 max-w-xl flex flex-col space-y-2'>
+      <form className="container px-4 mx-auto pt-24 mt-8 max-w-xl flex flex-col space-y-6" onSubmit={fetchCoordinates}>
+        <img src={WeatherSenseLogo}/>
+        <div className="rounded py-2 px-4 flex border-2 border-gray-300">
+          <div className="place-self-center text-gray-500 cursor-pointer">
+            <UilSearch/>
+          </div>
+          <div className='flex px-4 w-full'>
+            <input className="w-full border-none outline-none text-base" placeholder="Enter Location or Post Code" type="text" onChange={(e) => setInput(e.target.value)} />
+          </div>
+          <div className="place-self-center text-gray-500 cursor-pointer">
+            <UilLocationPinAlt/>
+          </div>
         </div>
-        <div className="button"> 
+        <div className="mx-auto flex space-x-2">
           {searchButton}
-          <button type="button">Current Location</button>
+          <button className="px-4 py-2 rounded bg-gray-300 transition-all duration-500 hover:shadow-md" type="button">Current Location</button>
         </div>
       </form>
     </div>
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -4,10 +4,10 @@ import Forecast from './Forecast.jsx'
 
 const Weather = ({currentWeatherData, forecastWeatherData}) => {
   return (
-    <>
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
     <CurrentWeather currentWeatherData={currentWeatherData}/>
     <Forecast forecastWeatherData={forecastWeatherData}/>
-    </>
+    </div>
   )
 }
 
