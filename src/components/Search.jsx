37558142fb80e9bbc3ea990b7e7b3881aa31f53f
mohammed.mahboob1 <mohammed.mahboob1@hscic.gov.uk>
2023-10-11 10:55:22 +0100

    Installed tailwind css to react project

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,32 +1,30 @@
 import React from 'react'
+import Inputs from './Inputs'
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
 
-const LocationWeatherSearch = ({fetchCoordinates, setInput, input}) => {
-  
+const LocationWeatherSearch = ({ fetchCoordinates, setInput, input }) => {
+
   let searchButton
 
-  if(input === ''){
+  if (input === '') {
     searchButton = <button type="submit" disabled>Search</button>
   } else {
     searchButton = <button type="submit">Search</button>
   }
 
-  return(
-    <>
-     <div>LocationWeatherSearch</div>
-     <section className='search-wrapper'>
-       <div>WeatherSense Logo</div>
-       <form onSubmit={fetchCoordinates}>
-        <img className='logo' src='assets/searching-magnifying-glass.png' alt='logo'/>
+  return (
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+      <Inputs />
+      <form onSubmit={fetchCoordinates}>
         <div>
-          <input placeholder="Enter location or postcode" type="text" onChange={(e) => setInput(e.target.value)}/>
+          <input placeholder="Enter location or postcode" type="text" onChange={(e) => setInput(e.target.value)} />
         </div>
-        <div className="button">
+        <div className="button"> 
           {searchButton}
           <button type="button">Current Location</button>
         </div>
-       </form>
-     </section>
-    </>
+      </form>
+    </div>
   )
 }
 
