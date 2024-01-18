413d051eeafb4d7fe858e9eaa44c5eddd4a3a577
mohammed.mahboob1 <mohammed.mahboob1@hscic.gov.uk>
2024-01-18 12:49:46 +0000

    wrapped root react app component with apolloprovider

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ client
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
 );
