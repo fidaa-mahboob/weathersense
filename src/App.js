eeca57200a40da89bb0e0a0fb3e2f3c5dc23cc5f
fidaa-mahboob <fidaa_786@hotmail.com>
2024-07-23 13:00:24 +0100

    Improved search page content layout.

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   let lon
 
   const getCurrentLocationWeather = () => {
-    if(navigator.geolocation){  
+    if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(async (pos) => {
         lat = pos.coords.latitude
         lon = pos.coords.longitude
@@ -25,21 +25,21 @@ function App() {
         await Axios.get(
           `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=9febc35812425cf718ad7e6c9ba49d6f&units=metric`
         ).then((res) => { setCurrentWeatherData({ ...res.data }) })
-  
+
         await Axios.get(
           `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=9febc35812425cf718ad7e6c9ba49d6f&units=metric`
         ).then((res) => {
           setForecastWeatherData({ ...res.data })
-        }).then(() => {setDataLoaded(true)})
+        }).then(() => { setDataLoaded(true) })
       })
-    } 
+    }
   }
 
   const fetchWeatherData = async (e) => {
     e.preventDefault()
     try {
       let response
-    
+
       // line 34 tests input for valid GB Post Code format
       if (/^[A-Za-z]{1,2}[0-9]{1,2}[A-Za-z]?[0-9][A-Za-z]{2}$/.test(input) || /^[A-Za-z]{1,2}[0-9]{1,2}[A-Za-z]?\s[0-9][A-Za-z]{2}$/.test(input)) {
         response = await Axios.get(
@@ -63,19 +63,27 @@ function App() {
         `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=9febc35812425cf718ad7e6c9ba49d6f&units=metric`
       ).then((res) => {
         setForecastWeatherData({ ...res.data })
-      }).then(() => {setDataLoaded(true)})
+      }).then(() => { setDataLoaded(true) })
     } catch (err) {
       console.log(err)
     }
   }
 
   return (
-    <div >
-      {
-        dataLoaded ?
-          <Weather currentWeatherData={currentWeatherData} forecastWeatherData={forecastWeatherData} /> :
-          <Search fetchWeatherData={fetchWeatherData} setInput={setInput} input={input} currentLocationWeather={getCurrentLocationWeather}/>
-      }
+    <div className="relative flex flex-col h-screen">
+      <main className='container mx-auto max-w-7xl pt-1 px-1 flex-grow'>
+        {
+          dataLoaded ?
+            <Weather currentWeatherData={currentWeatherData} forecastWeatherData={forecastWeatherData} /> :
+            <Search fetchWeatherData={fetchWeatherData} setInput={setInput} input={input} currentLocationWeather={getCurrentLocationWeather} />
+        }
+      </main>
+      <footer className="w-full flex items-center justify-center py-3">
+        <div className="md:flex md:items-end md:justify-center">
+          <span className=" text-gray-500 sm:text-center dark:text-gray-400">© 2023 WeatherSense. All Rights Reserved. | Powered by AWS
+          </span>
+        </div>
+      </footer>
     </div>
   )
 }
