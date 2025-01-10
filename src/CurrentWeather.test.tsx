ba86913951c0f4561ef1f25ba59eabfb86d9dcb2
fidaa-mahboob <fidaa_786@hotmail.com>
2025-01-10 12:01:02 +0000

    Added the mock for the react icons representing weather information

--- src/CurrentWeather.test.tsx
+++ src/CurrentWeather.test.tsx
@@ -6,5 +6,13 @@ jest.mock("src/components/TimeAndLocation", () => ({
     TimeAndLocation: () => <div data-testid="time-and-location">Time and Location</div>,
 }));
 
+jest.mock("@iconscout/react-unicons", () => ({
+    UilTemperature: () => <span data-testid="uil-temperature" />,
+    UilTear: () => <span data-testid="uil-tear" />,
+    UilWind: () => <span data-testid="uil-wind" />,
+    UilArrowUp: () => <span data-testid="uil-arrow-up" />,
+    UilArrowDown: () => <span data-testid="uil-arrow-down" />,
+  }));
+
 
 
