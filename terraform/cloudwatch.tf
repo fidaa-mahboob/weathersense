edd29bac768996efc302b1a82760e658a947069a
fidaa-mahboob <fidaa_786@hotmail.com>
2025-02-25 11:21:43 +0000

    Added the 4xx cloudwatch alarm

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -11,6 +11,7 @@ resource "aws_cloudwatch_metric_alarm" "cloudfront_5xx_alarm" {
         Region = "Global"
     }
 
+    alarm_description = "Triggers when CloudFront 5XX errors exceed 5%."
     actions_enabled = true
     alarm_actions = [aws_sns_topic.weathersense_cloudfront_alert]
 }
@@ -25,3 +26,20 @@ resource "aws_sns_topic_subscription" "email_subscriber" {
     endpoint = "fidaamahboob@gmail.com"
 }
 
+resource "aws_cloudwatch_metric_alarm" "cloudfront_4xx_alarm" {
+    alarm_name = "Cloudwatch-4xx-Errors"
+    comparison_operator = "GreaterThanThreshold" 
+    evaluation_periods = 1
+    metric_name = "4xxErrorRate"
+    namespace = "AWS/Cloudfront"
+    threshold = 10
+
+    dimensions = {
+        DistributionId = aws_cloudfront_distribution.website_cdn.id
+        Region = "Global"
+    }
+
+    alarm_description = "Triggers when CloudFront 4XX errors exceed 10%."
+    actions_enabled = true
+    alarm_actions = [aws_sns_topic.weathersense_cloudfront_alert]
+}
