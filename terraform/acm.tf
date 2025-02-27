4e4a38f9c2b0d3c8274f26bfef8a539f27637775
fidaa-mahboob <fidaa_786@hotmail.com>
2025-02-27 10:03:59 +0000

    ran terraform fmt command to correct file format

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -1,8 +1,8 @@
 resource "aws_acm_certificate" "default" {
-  provider          = aws.acm-provider
-  domain_name       = var.domain_name
-  subject_alternative_names = [ "*.${var.domain_name}" ]
-  validation_method = "DNS"
+  provider                  = aws.acm-provider
+  domain_name               = var.domain_name
+  subject_alternative_names = ["*.${var.domain_name}"]
+  validation_method         = "DNS"
 
   lifecycle {
     create_before_destroy = true
--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -1,68 +1,68 @@
 resource "aws_cloudwatch_metric_alarm" "cloudfront_5xx_alarm" {
-    alarm_name = "Cloudwatch-5xx-Errors"
-    comparison_operator = "GreaterThanThreshold" 
-    evaluation_periods = 1
-    metric_name = "5xxErrorRate"
-    namespace = "AWS/Cloudfront"
-    threshold = 5
-    statistic = "Average"
+  alarm_name          = "Cloudwatch-5xx-Errors"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "5xxErrorRate"
+  namespace           = "AWS/Cloudfront"
+  threshold           = 5
+  statistic           = "Average"
 
-    dimensions = {
-        DistributionId = aws_cloudfront_distribution.website_cdn.id
-        Region = "Global"
-    }
+  dimensions = {
+    DistributionId = aws_cloudfront_distribution.website_cdn.id
+    Region         = "Global"
+  }
 
-    alarm_description = "Triggers when CloudFront 5XX errors exceed 5%."
-    actions_enabled = true
-    alarm_actions = [aws_sns_topic.weathersense_cloudfront_alert.arn]
+  alarm_description = "Triggers when CloudFront 5XX errors exceed 5%."
+  actions_enabled   = true
+  alarm_actions     = [aws_sns_topic.weathersense_cloudfront_alert.arn]
 }
 
 resource "aws_sns_topic" "weathersense_cloudfront_alert" {
-    name = "weathersense-cloudfront-alert-topic"
+  name = "weathersense-cloudfront-alert-topic"
 }
 
 resource "aws_sns_topic_subscription" "email_subscriber" {
-    topic_arn = aws_sns_topic.weathersense_cloudfront_alert.arn
-    protocol = "email"
-    endpoint = "fidaamahboob@gmail.com"
+  topic_arn = aws_sns_topic.weathersense_cloudfront_alert.arn
+  protocol  = "email"
+  endpoint  = "fidaamahboob@gmail.com"
 }
 
 resource "aws_cloudwatch_metric_alarm" "cloudfront_4xx_alarm" {
-    alarm_name = "Cloudwatch-4xx-Errors"
-    comparison_operator = "GreaterThanThreshold" 
-    evaluation_periods = 1
-    metric_name = "4xxErrorRate"
-    namespace = "AWS/Cloudfront"
-    threshold = 10
-    statistic = "Average"
+  alarm_name          = "Cloudwatch-4xx-Errors"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "4xxErrorRate"
+  namespace           = "AWS/Cloudfront"
+  threshold           = 10
+  statistic           = "Average"
 
-    dimensions = {
-        DistributionId = aws_cloudfront_distribution.website_cdn.id
-        Region = "Global"
-    }
+  dimensions = {
+    DistributionId = aws_cloudfront_distribution.website_cdn.id
+    Region         = "Global"
+  }
 
-    alarm_description = "Triggers when CloudFront 4XX errors exceed 10%."
-    actions_enabled = true
-    alarm_actions = [aws_sns_topic.weathersense_cloudfront_alert.arn]
+  alarm_description = "Triggers when CloudFront 4XX errors exceed 10%."
+  actions_enabled   = true
+  alarm_actions     = [aws_sns_topic.weathersense_cloudfront_alert.arn]
 }
 
 resource "aws_cloudwatch_metric_alarm" "cloudfront_latency_alarm" {
-    alarm_name = "Cloudwatch-High-Latency"
-    comparison_operator = "GreaterThanThreshold" 
-    evaluation_periods = 1
-    metric_name = "TotalResponseTime"
-    namespace = "AWS/Cloudfront"
-    threshold = 0.5
-    period = 300
-    statistic = "Average"
-    treat_missing_data = "notBreaching"
+  alarm_name          = "Cloudwatch-High-Latency"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "TotalResponseTime"
+  namespace           = "AWS/Cloudfront"
+  threshold           = 0.5
+  period              = 300
+  statistic           = "Average"
+  treat_missing_data  = "notBreaching"
 
-    dimensions = {
-        DistributionId = aws_cloudfront_distribution.website_cdn.id
-        Region = "Global"
-    }
+  dimensions = {
+    DistributionId = aws_cloudfront_distribution.website_cdn.id
+    Region         = "Global"
+  }
 
-    alarm_description = "Triggers when CloudFront response time exceeds 500ms."
-    actions_enabled = true
-    alarm_actions = [aws_sns_topic.weathersense_cloudfront_alert.arn]
+  alarm_description = "Triggers when CloudFront response time exceeds 500ms."
+  actions_enabled   = true
+  alarm_actions     = [aws_sns_topic.weathersense_cloudfront_alert.arn]
 }
