90fbdab4ffb6108063ac3345e4bc7c19e96880f3
fidaa-mahboob <fidaa_786@hotmail.com>
2025-03-01 10:05:43 +0000

    refined waf terraform config and corrected error in waf statement block

--- terraform/waf.tf
+++ terraform/waf.tf
@@ -1,12 +1,11 @@
-# Create AWS WAF Web ACL
 resource "aws_wafv2_web_acl" "weathersense_waf" {
   provider    = aws.acm-provider
   name        = "weathersense-waf"
-  description = "WAF to protect Weather Sense CloudFront"
+  description = "WAF to protect WeatherSense CloudFront"
   scope       = "CLOUDFRONT"
 
   default_action {
-    allow {}
+    allow {} 
   }
 
   visibility_config {
@@ -15,19 +14,34 @@ resource "aws_wafv2_web_acl" "weathersense_waf" {
     sampled_requests_enabled   = true
   }
 
+  # Example rule: Block common attacks
   rule {
     name     = "AWS-CommonRuleSet"
     priority = 1
 
-    statement {
-      managed_rule_group_statement {
-        vendor_name = "AWS"
-        name        = "AWSManagedRulesCommonRuleSet"
-      }
+    override_action {
+      count {}
     }
 
-    action {
-      allow {}
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+
+        rule_action_override {
+          action_to_use {
+            count {}
+          }
+          name = "SizeRestrictions_QUERYSTRING"
+        }
+
+        rule_action_override {
+          action_to_use {
+            count {}
+          }
+          name = "NoUserAgent_HEADER"
+        }
+      }
     }
 
     visibility_config {
@@ -38,12 +52,41 @@ resource "aws_wafv2_web_acl" "weathersense_waf" {
   }
 
   rule {
-    name     = "AWS-BadInputsRuleSet"
-    priority = 2
-    action {
+    name     = "AWS-BotControlRuleSet"
+    priority = 3
+
+    override_action {
       count {}
     }
 
+    statement {
+      managed_rule_group_statement {
+        vendor_name = "AWS"
+        name        = "AWSManagedRulesBotControlRuleSet"
+
+        rule_action_override {
+          action_to_use {
+            block {}
+          }
+          name = "CategoryKnownBotContent"
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "bot-control"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "AWS-BadInputsRuleSet"
+    priority = 2
+
+    override_action {
+      count {}
+    }
     statement {
       managed_rule_group_statement {
         vendor_name = "AWS"
@@ -53,28 +96,6 @@ resource "aws_wafv2_web_acl" "weathersense_waf" {
     visibility_config {
       cloudwatch_metrics_enabled = true
       metric_name                = "bad-inputs"
-      sampled_requests_enabled   = true
-    }
-  }
-
-  rule {
-    name     = "AWS-BotControlRuleSet"
-    priority = 3
-
-    statement {
-      managed_rule_group_statement {
-        vendor_name = "AWS"
-        name        = "AWSManagedRulesBotControlRuleSet"
-      }
-    }
-
-    action {
-      block {}
-    }
-
-    visibility_config {
-      cloudwatch_metrics_enabled = true
-      metric_name                = "bot-control"
       sampled_requests_enabled   = true
     }
   }
